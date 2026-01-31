1facfc1c0998089d9083bc4e537f69144f794bb5
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-01-31 21:32:31 +0300

    pkg/analyzer: removes the dependency on `inspect.Analyzer` as it was neither in use nor would create meaningful performance increase as the golistics only iterates over function declarations using ast just with similar overhead

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -3,14 +3,12 @@ package analyzer
 import (
 	"go.ufukty.com/golistics/internal/golistics"
 	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
 )
 
 func New() *analysis.Analyzer {
 	return &analysis.Analyzer{
-		Name:     "golistics",
-		Doc:      "method linter to remind missing fields",
-		Run:      golistics.Inspect,
-		Requires: []*analysis.Analyzer{inspect.Analyzer},
+		Name: "golistics",
+		Doc:  "method linter to remind missing fields",
+		Run:  golistics.Inspect,
 	}
 }
